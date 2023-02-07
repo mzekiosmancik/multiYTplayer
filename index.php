c6edb7fde084c2fe6b4a7fb30fa5ef01e9d41b43
Mezo Osmancık <mzekiosmancik@outlook.com>
2023-02-07 00:18:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ $autoplay = !isset($_GET["autoplay"]) || $_GET["autoplay"] == "on" ? 1 : 0;
                                 <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
                             </svg>
                         </span>
-                     <?php   <input type="text" aria-label="Channel Address" placeholder="Channel Address" name="cs[]" value="<?php echo $cs;?>" class="form-control rounded-0">
+                     <input type="text" aria-label="Channel Address" placeholder="Channel Address" name="cs[]" value="<?php echo $cs;?>" class="form-control rounded-0">
                     </div>
 <?php } ?> 
                 </div>
